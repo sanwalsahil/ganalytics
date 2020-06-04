9c6848adefbd64fb125fc125f71ab2153d7139f3
sahil sanwal <sanwalsahil2017@gmail.com>
2020-06-04 09:56:21 +0530

    new commit

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168463022-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-168463022-1');
+    </script>
+
+</head>
+<body>
+    hello
+</body>
+</html>
